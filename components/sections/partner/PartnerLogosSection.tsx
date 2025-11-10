4995fd6a3eb94a459e44304fd0ffc3ee85b78674
DatMV <datmv01@gmail.com>
2025-11-10 21:55:45 +0700

    feat(partner): implement partner logos

--- components/sections/partner/PartnerLogosSection.tsx
+++ components/sections/partner/PartnerLogosSection.tsx
@@ -1,13 +1,27 @@
 import { Container } from "@/components";
+import { assets } from "@/constants";
+import clsx from "clsx";
 
 const PartnerLogosSection = () => {
   return (
-    <Container
-      as="section"
-      className="min-h-screen flex justify-center items-center"
-      id="partner-logos"
-    >
-      PartnerLogosSection
+    <Container as="section" className="flex flex-col" id="partner-logos">
+      <div data-aos="fade-right" className="mt-10">
+        <h2 className="text-xs font-medium uppercase font-inter text-primary-900">
+          Có mặt tại
+        </h2>
+        <h1 className="mt-2 text-2xl tracking-wide capitalize lg:text-3xl font-lobster">
+          Những Nhà Phân Phối
+          <span className=" bg-clip-text text-transparent bg-linear-to-l from-primary-500 to-primary-900 before:content-['_'] ">
+            Uy Tín
+          </span>
+        </h1>
+      </div>
+
+      <div className={clsx("flex gap-4 mt-9 md:mt-16", "animate-moveLeft")}>
+        {assets.partnerLogos.map((logo, idx) => (
+          <img key={idx} src={logo.src} alt={logo.alt} className="h-10" />
+        ))}
+      </div>
     </Container>
   );
 };
--- constants/assets.ts
+++ constants/assets.ts
@@ -7,20 +7,36 @@ import hero_img from "@/public/assets/hero_img.png";
 import logo from "@/public/assets/logo.png";
 import rightArrow from "@/public/assets/right-arrow.svg";
 
-
- const images = {
+const images = {
   banner1,
   banner2,
   banner3,
   banner4,
-  hero_img
+  hero_img,
 };
 
 const icons = {
   logo,
-  rightArrow
+  rightArrow,
 };
 
-const assets = { images, icons };
+export const partnerLogoBasePath = "/assets/partner-logos/";
+export const _partnerLogos = [
+  { src: partnerLogoBasePath + "costco.png", alt: "Costco Logo" },
+  { src: partnerLogoBasePath + "kroger.png", alt: "Kroger Logo" },
+  { src: partnerLogoBasePath + "meijer.png", alt: "Meijer Logo" },
+  { src: partnerLogoBasePath + "publix.png", alt: "Publix Logo" },
+  { src: partnerLogoBasePath + "safeway.png", alt: "Safeway Logo" },
+  { src: partnerLogoBasePath + "target.png", alt: "Target Logo" },
+  { src: partnerLogoBasePath + "walmart.png", alt: "Walmart Logo" },
+  { src: partnerLogoBasePath + "wegmans.png", alt: "Wegmans Logo" },
+  {
+    src: partnerLogoBasePath + "whole-foods.png",
+    alt: "Whole Foods Logo",
+  },
+];
+const partnerLogos = [_partnerLogos, ..._partnerLogos, ..._partnerLogos];
+
+const assets = { images, icons, partnerLogos };
 
 export default assets;
